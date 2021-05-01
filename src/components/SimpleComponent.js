c9966b4a7a6f050831255c443ab80d2ef49d6434
Xiaochen Xu <xiaochex@gmail.com>
2021-04-30 22:02:03 -0400

    commit tree 26e92a3551de804fc1e4c3926bedd281ac927241

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,24 @@
 // Code SimpleComponent Here
+import React, { Component } from 'react'
+
+export default class SimpleComponent extends Component {
+    state = {
+        mood: 'happy'
+    }
+
+
+handleClick = (e) => {
+    this.setState({
+        mood: 'sad'
+    })
+
+}
+
+    render() {
+        return (
+            <div onClick={this.handleClick}>
+                {this.state.mood}
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+const superHappy = (e) => {
+  alert('surprise!')
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={superHappy} />
   </div>,
   document.getElementById('root')
 );
